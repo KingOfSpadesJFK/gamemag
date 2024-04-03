f0e5e20881f2a3dc627e25de78cfa39359fb2e7d
KingOfSpadesJFK <kingofspadeslm567@gmail.com>
2024-04-03 01:52:47 -0700

    Update Camera.cs

--- script/Camera.cs
+++ script/Camera.cs
@@ -3,9 +3,12 @@ using System;
 
 public partial class Camera : Camera2D
 {
+	private Node _parent;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_parent = GetParent<Node>();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,10 +19,13 @@ public partial class Camera : Camera2D
 											"debug.camera_up",
 											"debug.camera_down");
 		if (direction != Vector2.Zero) {
+			Reparent(_parent.GetParent());
 			Position += direction * (Input.IsActionPressed("debug.camera_speed") ? 5.0f : 2.5f);
 		}
 		if (Input.IsActionJustPressed("debug.camera_reset")) {
+			Reparent(_parent);
 			Position = Vector2.Zero;
 		}
+		Position = Position.Round();
 	}
 }
